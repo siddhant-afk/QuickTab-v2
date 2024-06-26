854bd69815e6880fc3f9334cd0318cbf2606c447
siddhant-afk <siddhantdudhane21@gmail.com>
2024-06-26 23:12:32 +0530

    Add avatar in Navbar component and icons in Sidebar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
+import avatar from  "../assets/images/IMG_0212.jpg";
+import { IoChevronDownOutline } from "react-icons/io5";;
+
+ 
+
 function Navbar(){
 
     return(
       
-        <section className="navbar ps-auto text-slate-400">
-          <ul className="flex justify-end px-10">
-            <li>Account</li>
-            <li>Settings</li>
-          </ul>
+        <section className="navbar flex  pr-10 text-white flex justify-end">
+          <div className="flex avatar">
+            <img className="w-10 h-10 rounded-full" src={avatar} alt="Rounded avatar" /> 
+            <h5 className=" px-2 my-2">Siddhant Dudhane <IoChevronDownOutline className="inline"/></h5>
+          </div>
+          
         </section>
     )
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import TabButton from "./TabButton";
 import { IoIosAddCircle } from "react-icons/io";
- 
+import { IoHomeOutline } from "react-icons/io5";
+import { BsPeople } from "react-icons/bs";
+import { BsCart3 } from "react-icons/bs";
+import { TbInvoice } from "react-icons/tb";
+import { LiaFileInvoiceSolid } from "react-icons/lia";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
+
 function Sidebar() {
  const [Tab,selectTab] = useState("Dashboard");
 
@@ -11,17 +17,17 @@ function Sidebar() {
  }
     return (
         <div className="sidebar">
-        <div className="brand-heading py-4 px-10">
-              <h1 className="text-xl text-slate-400">QuickTab</h1>
+        <div className="brand-heading py-4 text-2xl text-white font-bold ps-10 flex">
+        <LiaFileInvoiceDollarSolid className="my-2" />  <h1 className="ms-2 my-1">QuickTab</h1>
             </div>
         
         <div className="sidebar-content">
         <ul className="tabs">
-                <TabButton isSelected = {Tab == "Dashboard"} onClick = {()=> handleClick("Dashboard")}>Dashboard</TabButton>
-                <TabButton isSelected = {Tab == "Customers"} onClick = {()=> handleClick("Customers")}>Customers</TabButton>
-                <TabButton isSelected = {Tab == "Items"} onClick = {()=> handleClick("Items")}>Items</TabButton>
-                <TabButton isSelected = {Tab == "Invoices"} onClick = {()=> handleClick("Invoices")}>Invoices</TabButton>
-                <TabButton isSelected = {Tab == "Expenses"} onClick = {()=> handleClick("Expenses")}>Expenses</TabButton>
+                <TabButton isSelected = {Tab == "Dashboard"} onClick = {()=> handleClick("Dashboard")}> <IoHomeOutline className="sidebar-icon"/> Dashboard</TabButton>
+                <TabButton isSelected = {Tab == "Customers"} onClick = {()=> handleClick("Customers")}><BsPeople className="sidebar-icon"/> Customers</TabButton>
+                <TabButton isSelected = {Tab == "Items"} onClick = {()=> handleClick("Items")}><BsCart3 className="sidebar-icon"/> Items</TabButton>
+                <TabButton isSelected = {Tab == "Invoices"} onClick = {()=> handleClick("Invoices")}><LiaFileInvoiceSolid className="sidebar-icon"/> Invoices</TabButton>
+                <TabButton isSelected = {Tab == "Expenses"} onClick = {()=> handleClick("Expenses")}> <TbInvoice className="sidebar-icon"/> Expenses</TabButton>
                
             </ul>
         </div>
